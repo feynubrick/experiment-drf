2b8417116c1a2eecf64753775480e2baa7d7dade
Seung-Yoon Oh <feynubrick@proton.me>
2023-03-07 23:36:13 +0900

    nginx 설정 다듬기 + proxy 서버가 있으니 local에서 접근하는 host는 모두 허용하도록 변경

--- experiment/settings.py
+++ experiment/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = env_vars.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env_vars.DEBUG
 
-ALLOWED_HOSTS = [env_vars.HOST]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
 
 
 # Application definition
