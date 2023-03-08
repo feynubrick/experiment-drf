da483ed81de91c98778faf833ba06884a30b0e2c
Seung-Yoon Oh <feynubrick@proton.me>
2023-03-08 23:02:27 +0900

    환경 별로 다른 명령어 실행되도록 변경. local에서는 nginx 사용 안하도록 변경

--- run.sh
+++ run.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
 #python manage.py runserver $DJANGO_HOST:$DJANGO_PORT
-gunicorn --bind :$DJANGO_PORT experiment.wsgi:application
+if [ "$RUN_ENV" == "prod" ]; then
+  echo "============================"
+  echo "<< PRODUCTION ENVIRONMENT >>"
+  echo "============================"
+  gunicorn --bind :"$DJANGO_PORT" experiment.wsgi:application
+elif [ "$RUN_ENV" == "local" ]; then
+  echo "======================="
+  echo "<< LOCAL ENVIRONMENT >>"
+  echo "======================="
+  python manage.py runserver "$DJANGO_HOST:$DJANGO_PORT"
+else
+  echo "ERROR: '$RUN_ENV' IS INVALID RUN_ENV" >&2
+  exit 1
+fi
